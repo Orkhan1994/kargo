e3d23675371201dec3c76f27ec136a7b61d9a10b
Orkhan Mirzayev <orxanmirzeyev676@gmail.com>
2022-01-26 23:12:04 +0400

    create AboutUsRepo

---
+++ src/main/java/com/example/kargo/repository/AboutUsRepo.java
@@ -0,0 +1,8 @@
+package com.example.kargo.repository;
+
+import com.example.kargo.entity.AboutUs;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface AboutUsRepo extends JpaRepository<AboutUs,Long> {
+}
