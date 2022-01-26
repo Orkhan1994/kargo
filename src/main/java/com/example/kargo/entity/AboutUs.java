f363e3350e59339609a213f5c3f34e7a04df00aa
Orkhan Mirzayev <orxanmirzeyev676@gmail.com>
2022-01-26 23:10:31 +0400

    Create  entity class

---
+++ src/main/java/com/example/kargo/entity/AboutUs.java
@@ -0,0 +1,24 @@
+package com.example.kargo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "about_us")
+public class AboutUs {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "description")
+    private String description;
+
+}
---
+++ src/main/java/com/example/kargo/entity/Roles.java
@@ -0,0 +1,4 @@
+package com.example.kargo.entity;
+
+public class Roles {
+}
